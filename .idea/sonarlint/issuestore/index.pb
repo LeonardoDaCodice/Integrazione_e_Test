78a554baf86101dc4ccfb9f31e477edc8f721c02
LeonardoDaCodice <s.forte16@gmail.com>
2023-10-13 09:03:07 +0200

    HW3

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+s
+CCodicePerHomework2/src/main/java/org/example/StringManipulator.java,7\1\715273ec017c0a1eb14ded6ee5d41c50e3bc5b19
+w
+GCodicePerHomework2/src/test/java/org/example/StringManipulatorTest.java,2\6\26ad8670c6936d33f663d030d89bf32c81a52426
+f
+6CodicePerHomework2/src/main/java/org/example/Main.java,4\8\4899dcbbb3e5f54d4e8e6638cb5685cf5a2f0d04
---
+++ HW3/src/test/java/CodeHW3Test.java
@@ -0,0 +1,71 @@
+import net.jqwik.api.*;
+import net.jqwik.api.arbitraries.IntegerArbitrary;
+import net.jqwik.api.statistics.Histogram;
+import net.jqwik.api.statistics.Statistics;
+import net.jqwik.api.statistics.StatisticsReport;
+import org.example.CodeHW3;
+import org.junit.jupiter.api.Assertions;
+
+public class CodeHW3Test {
+    @Provide
+    IntegerArbitrary validInteger() {
+        return Arbitraries.integers().between(-20, 20);
+    }
+
+
+    @Property
+    @Report(Reporting.GENERATED)
+    @StatisticsReport(format = Histogram.class)
+    void evenOrOddTest(@ForAll("validInteger") int value) {
+        CodeHW3 hw3 = new CodeHW3();
+        boolean result = hw3.evenOrOdd(value);
+        boolean isEven = isEven(value);
+
+        Assertions.assertEquals(isEven, result);
+        Statistics.label("ExpectedResult").collect(isEven);
+        Statistics.label("OurMethod").collect(result);
+    }
+
+
+    @Property
+    @Report(Reporting.GENERATED)
+    @StatisticsReport(format = Histogram.class)
+    void isPrimeTest(@ForAll("validInteger") int value) {
+        CodeHW3 hw3 = new CodeHW3();
+        boolean result = hw3.isPrime(value);
+        boolean isPrime = isPrimeCheck(value);
+
+        Assertions.assertEquals(isPrime, result);
+        Statistics.label("ExpectedResult").collect(isPrime);
+        Statistics.label("OurMethod").collect(result);
+    }
+
+
+    private boolean isEven(int value){
+        if (value % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isPrimeCheck(int value) {
+        if (value <= 1) {
+            return false;
+        }
+        if (value <= 3) {
+            return true;
+        }
+        if (value % 2 == 0 || value % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= value; i += 6) {
+            if (value % i == 0 || value % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
